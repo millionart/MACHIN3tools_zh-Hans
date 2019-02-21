837e2303cbb35265853abb0baae526de67b26cef
machin3io <social@machin3.io>
2019-02-21 20:07:05 +0100

    PieSave() - add incremental save path to recent files list

---
+++ utils/system.py
@@ -0,0 +1,17 @@
+import bpy
+
+
+def add_path_to_recent_files(path):
+    """
+    add the path to the recent files list, for some reason it's not done automatically when saving or loading
+    """
+
+    try:
+        recent_path = bpy.utils.user_resource('CONFIG', "recent-files.txt")
+        with open(recent_path, "r+") as f:
+            content = f.read()
+            f.seek(0, 0)
+            f.write(path.rstrip('\r\n') + '\n' + content)
+
+    except (IOError, OSError, FileNotFoundError):
+        pass
