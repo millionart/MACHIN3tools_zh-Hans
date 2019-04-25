9ac884ca4ccc13ba91b2df4d79f4cb51399f5676
machin3io <social@machin3.io>
2019-04-25 02:47:57 +0200

    Shading Pie, Toggle Smooth and Toggle Flat - add alternative modes

--- ui/operators/shade_smooth_flat_auto.py
+++ ui/operators/shade_smooth_flat_auto.py
@@ -15,21 +15,21 @@ class ShadeSmooth(bpy.types.Operator):
 
             # set sharps based on face angles + activate auto smooth + enable sharp overlays
             if event.alt:
-                active = context.active_object
-                active.data.use_auto_smooth = True
-                angle = active.data.auto_smooth_angle
+                for obj in context.selected_objects:
+                    obj.data.use_auto_smooth = True
+                    angle = obj.data.auto_smooth_angle
 
-                bm = bmesh.new()
-                bm.from_mesh(active.data)
-                bm.normal_update()
+                    bm = bmesh.new()
+                    bm.from_mesh(obj.data)
+                    bm.normal_update()
 
-                sharpen = [e for e in bm.edges if e.calc_face_angle() > angle]
+                    sharpen = [e for e in bm.edges if e.calc_face_angle() > angle]
 
-                for e in sharpen:
-                    e.smooth = False
+                    for e in sharpen:
+                        e.smooth = False
 
-                bm.to_mesh(active.data)
-                bm.clear()
+                    bm.to_mesh(obj.data)
+                    bm.clear()
 
                 context.space_data.overlay.show_edge_sharp = True
 
@@ -42,12 +42,34 @@ class ShadeSmooth(bpy.types.Operator):
 class ShadeFlat(bpy.types.Operator):
     bl_idname = "machin3.shade_flat"
     bl_label = "Shade Flat"
-    bl_description = "Set flat shading in object and edit mode"
+    bl_description = "Set flat shading in object and edit mode\nALT: Clear all sharps, bweights, creases and seams."
     bl_options = {'REGISTER', 'UNDO'}
 
-    def execute(self, context):
+    def invoke(self, context, event):
         if context.mode == "OBJECT":
             bpy.ops.object.shade_flat()
+
+            # clear all sharps, bweights, seams and creases
+            if event.alt:
+                for obj in context.selected_objects:
+                    obj.data.use_auto_smooth = False
+
+                    bm = bmesh.new()
+                    bm.from_mesh(obj.data)
+                    bm.normal_update()
+
+                    bw = bm.edges.layers.bevel_weight.verify()
+                    cr = bm.edges.layers.crease.verify()
+
+                    for e in bm.edges:
+                        e[bw] = 0
+                        e[cr] = 0
+                        e.smooth = True
+                        e.seam = False
+
+                    bm.to_mesh(obj.data)
+                    bm.clear()
+
 
         elif context.mode == "EDIT_MESH":
             bpy.ops.mesh.faces_shade_flat()
