5f91c5e8a6c63c5c8c1dbd36e51bb502e4742daa
machin3io <social@machin3.io>
2019-04-25 03:25:59 +0200

    Shading Pie, toggleSmooth and ToggleFlat - add EDIT_MESH alt modes
    * in edit mode, the ALT modes act like in object mode, smoothing and
    flattenting everything instead of just the selection
    * this then means, there's no need to leave edit mode or do a global
    selection to do global smoothing/flattening and sharpening/unsharpening

--- ui/operators/shade_smooth_flat_auto.py
+++ ui/operators/shade_smooth_flat_auto.py
@@ -16,27 +16,54 @@ class ShadeSmooth(bpy.types.Operator):
             # set sharps based on face angles + activate auto smooth + enable sharp overlays
             if event.alt:
                 for obj in context.selected_objects:
-                    obj.data.use_auto_smooth = True
-                    angle = obj.data.auto_smooth_angle
-
-                    bm = bmesh.new()
-                    bm.from_mesh(obj.data)
-                    bm.normal_update()
-
-                    sharpen = [e for e in bm.edges if e.calc_face_angle() > angle]
-
-                    for e in sharpen:
-                        e.smooth = False
-
-                    bm.to_mesh(obj.data)
-                    bm.clear()
+                    self.set_obj_sharps(obj)
 
                 context.space_data.overlay.show_edge_sharp = True
 
         elif context.mode == "EDIT_MESH":
-            bpy.ops.mesh.faces_shade_smooth()
+            if event.alt:
+                self.set_mesh_sharps(context.active_object.data)
+
+                context.space_data.overlay.show_edge_sharp = True
+            else:
+                bpy.ops.mesh.faces_shade_smooth()
 
         return {'FINISHED'}
+
+    def set_obj_sharps(self, obj):
+        obj.data.use_auto_smooth = True
+        angle = obj.data.auto_smooth_angle
+
+        bm = bmesh.new()
+        bm.from_mesh(obj.data)
+        bm.normal_update()
+
+        sharpen = [e for e in bm.edges if e.calc_face_angle() > angle]
+
+        for e in sharpen:
+            e.smooth = False
+
+        bm.to_mesh(obj.data)
+        bm.clear()
+
+    def set_mesh_sharps(self, mesh):
+        mesh.use_auto_smooth = True
+        angle = mesh.auto_smooth_angle
+
+        bm = bmesh.from_edit_mesh(mesh)
+        bm.normal_update()
+
+        # smooth all faces like in object mode
+        for f in bm.faces:
+            f.smooth = True
+
+
+        sharpen = [e for e in bm.edges if e.calc_face_angle() > angle]
+
+        for e in sharpen:
+            e.smooth = False
+
+        bmesh.update_edit_mesh(mesh)
 
 
 class ShadeFlat(bpy.types.Operator):
@@ -52,29 +79,56 @@ class ShadeFlat(bpy.types.Operator):
             # clear all sharps, bweights, seams and creases
             if event.alt:
                 for obj in context.selected_objects:
-                    obj.data.use_auto_smooth = False
-
-                    bm = bmesh.new()
-                    bm.from_mesh(obj.data)
-                    bm.normal_update()
-
-                    bw = bm.edges.layers.bevel_weight.verify()
-                    cr = bm.edges.layers.crease.verify()
-
-                    for e in bm.edges:
-                        e[bw] = 0
-                        e[cr] = 0
-                        e.smooth = True
-                        e.seam = False
-
-                    bm.to_mesh(obj.data)
-                    bm.clear()
-
+                    self.clear_obj_sharps(obj)
 
         elif context.mode == "EDIT_MESH":
-            bpy.ops.mesh.faces_shade_flat()
+            if event.alt:
+                self.clear_mesh_sharps(context.active_object.data)
+
+            else:
+                bpy.ops.mesh.faces_shade_flat()
 
         return {'FINISHED'}
+
+    def clear_obj_sharps(self, obj):
+        obj.data.use_auto_smooth = False
+
+        bm = bmesh.new()
+        bm.from_mesh(obj.data)
+        bm.normal_update()
+
+        bw = bm.edges.layers.bevel_weight.verify()
+        cr = bm.edges.layers.crease.verify()
+
+        for e in bm.edges:
+            e[bw] = 0
+            e[cr] = 0
+            e.smooth = True
+            e.seam = False
+
+        bm.to_mesh(obj.data)
+        bm.clear()
+
+    def clear_mesh_sharps(self, mesh):
+        mesh.use_auto_smooth = False
+
+        bm = bmesh.from_edit_mesh(mesh)
+        bm.normal_update()
+
+        bw = bm.edges.layers.bevel_weight.verify()
+        cr = bm.edges.layers.crease.verify()
+
+        # faltten all faces like in object mode
+        for f in bm.faces:
+            f.smooth = False
+
+        for e in bm.edges:
+            e[bw] = 0
+            e[cr] = 0
+            e.smooth = True
+            e.seam = False
+
+        bmesh.update_edit_mesh(mesh)
 
 
 class ToggleAutoSmooth(bpy.types.Operator):
