e5857d174e501ee75ada57a6cd573d7693ac4d53
machin3io <social@machin3.io>
2019-06-15 20:25:44 +0200

    Improve addon update proccess by properly reloading modules
    * simplify classes.py and keys.py into a single dicts.py

--- __init__.py
+++ __init__.py
@@ -29,6 +29,59 @@ bl_info = {
     "category": "Mesh"}
 
 
+def reload_modules():
+    """
+    This makes sure all modules are reloaded from new files, when the addon is removed and a new version is installed in the same session,
+    or when Blender's 'Reload Scripts' operator is run manually.
+    It's important, that utils modules are reloaded first, as operators and menus import from them
+    """
+
+    import os
+    import importlib
+
+    # first fetch and reload all utils modules
+    utils_modules = sorted([name[:-3] for name in os.listdir(os.path.join(__path__[0], "utils")) if name.endswith('.py')])
+
+    for module in utils_modules:
+        impline = "from . utils import %s" % (module)
+
+        print("reloading %s" % (".".join(['MACHIN3tools'] + ['utils'] + [module])))
+
+        exec(impline)
+        importlib.reload(eval(module))
+
+
+    # then update the classes and keys dicts
+    from . import dicts
+    importlib.reload(dicts)
+
+    # and based on that, reload the modules containing operator and menu classes
+    modules = []
+
+    for label in dicts.classes:
+        entries = dicts.classes[label]
+        for entry in entries:
+            path = entry[0].split('.')
+            module = path.pop(-1)
+
+            if (path, module) not in modules:
+                modules.append((path, module))
+
+    for path, module in modules:
+        if path:
+            impline = "from . %s import %s" % (".".join(path), module)
+        else:
+            impline = "from . import %s" % (module)
+
+        print("reloading %s" % (".".join(['MACHIN3tools'] + path + [module])))
+
+        exec(impline)
+        importlib.reload(eval(module))
+
+
+if 'bpy' in locals():
+    reload_modules()
+
 import bpy
 from bpy.props import PointerProperty
 from . properties import M3SceneProperties
