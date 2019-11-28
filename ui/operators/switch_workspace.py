d556a602a4706c1cadd73aaa56d989a37f2feb9d
machin3io <social@machin3.io>
2019-11-28 14:02:35 +0100

    SwitchWorkspace() - support toggling to alternative workspace

--- ui/operators/switch_workspace.py
+++ ui/operators/switch_workspace.py
@@ -1,6 +1,5 @@
 import bpy
 from bpy.props import StringProperty
-from ... utils import MACHIN3 as m3
 
 
 class SwitchWorkspace(bpy.types.Operator):
@@ -8,9 +7,20 @@ class SwitchWorkspace(bpy.types.Operator):
     bl_label = "Switch Workspace"
     bl_options = {'REGISTER'}
 
-    name: bpy.props.StringProperty()
+    name: StringProperty()
 
     def execute(self, context):
-        bpy.context.window.workspace = bpy.data.workspaces[self.name]
+        ws = bpy.data.workspaces.get(self.name)
+
+        # if the chosen workspace is already active, select the alternative one, if present
+        if ws and context.window.workspace == ws:
+            ws = bpy.data.workspaces.get('%s.alt' % self.name)
+
+            if ws:
+                bpy.context.window.workspace = ws
+
+        # otherwise just switch to the chosen one
+        elif ws:
+                bpy.context.window.workspace = ws
 
         return {'FINISHED'}
