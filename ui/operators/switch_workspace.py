9227f72b31521196934192e5bee953334b280e49
machin3io <social@machin3.io>
2019-12-09 23:37:31 +0100

    SwitchWorkspace()
    * transfer view_martrix from previous workspace's 3d view to the next

--- ui/operators/switch_workspace.py
+++ ui/operators/switch_workspace.py
@@ -10,7 +10,12 @@ class SwitchWorkspace(bpy.types.Operator):
     name: StringProperty()
 
     def execute(self, context):
+
+        # get current workspace
         ws = bpy.data.workspaces.get(self.name)
+
+        # get view matrix from 3d view (if present)
+        viewmx = self.get_view_matrix(context, ws)
 
         # if the chosen workspace is already active, select the alternative one, if present
         if ws and context.window.workspace == ws:
@@ -23,4 +28,22 @@ class SwitchWorkspace(bpy.types.Operator):
         elif ws:
                 bpy.context.window.workspace = ws
 
+        # set previous view matrix to new view
+        if viewmx:
+            self.set_view_matrix(ws, viewmx)
+
         return {'FINISHED'}
+
+    def set_view_matrix(self, workspace, viewmx):
+        for screen in workspace.screens:
+            for area in screen.areas:
+                if area.type == 'VIEW_3D':
+                    for space in area.spaces:
+                        if space.type == 'VIEW_3D':
+                            r3d = space.region_3d
+                            r3d.view_matrix = viewmx
+                            return
+
+    def get_view_matrix(self, context, workspace):
+        if context.space_data.type == 'VIEW_3D':
+            return context.space_data.region_3d.view_matrix
